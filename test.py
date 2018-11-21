51020a371beb6368c55bf180aa350690718285c2
spartanJS <alexandrephe@gmail.com>
2018-11-21 14:59:55 +0100

    Premier commit

---
+++ test.py
@@ -0,0 +1,24 @@
+#! usr/bin/python3
+# -*-coding:UTF-8-*
+class Etudiant:
+
+    """Classe représentant un étudiant.
+
+    On représente un étudiant par son prénom (attribut prenom), son âge
+    (attribut age) et sa note moyenne (attribut moyenne, entre 0 et 20).
+
+    Paramètres du constructeur :
+        prenom -- le prénom de l'étudiant
+        age -- l'âge de l'étudiant
+        moyenne -- la moyenne de l'étudiant
+
+    """
+
+    def __init__(self, prenom, age, moyenne):
+        self.prenom = prenom
+        self.age = age
+        self.moyenne = moyenne
+
+    def __repr__(self):
+        return "<Étudiant {} (âge={}, moyenne={})>".format(
+                self.prenom, self.age, self.moyenne)
